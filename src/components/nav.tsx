9382682f7099449300d7bf0ace38f741d9278231
Youngwoo Lee <youngwoo206@gmail.com>
2024-12-23 23:22:00 -0500

    init setup

---
+++ src/components/nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="w-full h-16 bg-background border-0">
+      <h1 className="text-4xl text-primary px-10">hello</h1>
+      <h1 className="text-4xl text-secondary px-10">hello</h1>
+      <h1 className="text-4xl text-accent px-10">hello</h1>
+    </div>
+  );
+}
+
+export default Nav;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        background: 'var(--background)',
+        accent: 'var(--accent)'
       },
+      fontFamily: {
+        open_sans: ["Open Sans", "sans-serif"]
+      }
     },
   },
   plugins: [],
